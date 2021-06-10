84382f3d67cfa967a63633ca4d21cbc933329dd0
Cotoc Vasile Gabriel <cotocvasilegabriel@yahoo.com>
2021-06-10 23:19:09 +0300

    Delete folder

--- baNaNa-master/public/script/responsiveActions.js
+++
@@ -1,4 +0,0 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const navBar = document.querySelector(".nav-bar");
-
-burgerMenu.addEventListener('click', () => navBar.classList.toggle("hidden-nav"));
